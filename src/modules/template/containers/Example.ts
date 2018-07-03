47ef4b385cfbb123132be5f6e1bd71f09764373d
Karl von Randow <karl@xk72.com>
2018-07-04 06:12:33 +1200

    Change capitalisation on Example container and component

--- src/modules/template/containers/Example.ts
+++ src/modules/template/containers/Example.ts
@@ -3,22 +3,22 @@
  */
 
  /* Import the component from the component path */
-import Component from '../components/example';
+import Component from '../components/example'
 
-import { connect } from 'react-redux';
-import { Dispatch } from 'redux';
+import { connect } from 'react-redux'
+import { Dispatch } from 'redux'
 
 /* Import RootStoreState */
-import { RootStoreState } from '../../index';
+import { RootStoreState } from '../../index'
 
 /* Import module actions */
-import * as actions from '../actions';
+import * as actions from '../actions'
 
 /**
  * Interface for properties that the container passes to the component.
  */
 export interface Props {
-    exampleProperty: string;
+	exampleProperty: string
 }
 
 /**
@@ -26,21 +26,21 @@ export interface Props {
  * The component's `this.props` is typed `Props & Actions`.
  */
 export interface Actions {
-    onExample: (value: string) => void;
+	onExample: (value: string) => void
 }
 
 /** Populate the Props from the store state. */
 const mapStateToProps = (state: RootStoreState): Props => {
-    return {
-        exampleProperty: state.template.name,
-    };
-};
+	return {
+		exampleProperty: state.template.name,
+	}
+}
 
 /** Populate the Actions with the callbacks for the component. */
 const mapDispatchToProps = (dispatch: Dispatch<{}>): Actions => ({
-    onExample: (value) => {
-        dispatch(actions.examplePrimitiveAction(value));
-    },
-});
+	onExample: (value) => {
+		dispatch(actions.examplePrimitiveAction(value))
+	},
+})
 
-export default connect(mapStateToProps, mapDispatchToProps)(Component);
+export default connect(mapStateToProps, mapDispatchToProps)(Component)
