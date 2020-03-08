38247108c78da95eb448a0eb10217d2a490ec55e
Karl von Randow <karl@xk72.com>
2020-03-09 12:47:03 +1300

    Update template files from create-react-app 3.4.0

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const App: React.FC = () => {
 				<header className="App-header">
 					<img src={logo} className="App-logo" alt="logo" />
 					<p>
-			Edit <code>src/App.tsx</code> and save to reload.
+					Edit <code>src/App.tsx</code> and save to reload.
 					</p>
 					<a
 						className="App-link"
@@ -20,7 +20,7 @@ const App: React.FC = () => {
 						target="_blank"
 						rel="noopener noreferrer"
 					>
-			Learn React
+					Learn React
 					</a>
 					<Example />
 				</header>
